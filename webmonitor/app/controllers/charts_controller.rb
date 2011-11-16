b9b1dd971a2b87b8f706a6af285e75e513ea9056
Almir 'm3nd3s <m3nd3s@gmail.com>
2011-11-16 03:26:07 -0200

    Lá vai a aplicação

--- webmonitor/app/controllers/charts_controller.rb
+++ webmonitor/app/controllers/charts_controller.rb
@@ -1,8 +1,19 @@
 class ChartsController < ApplicationController
 
   def show
-    sensor_id = params[:sensor_id] || 1
-    @temperatures = Temperature.last(15) #where("sensor_id = ? AND checked_at BETWEEN ? AND ?", sensor_id, Time.now - 12.hours, Time.now)
+    sensor_id = params[:sensor_id] || Sensor.first.id
+    if params[:start_at].nil? || params[:start_at].empty?
+      start_at =  Time.now - 12.hours
+    else
+      start_at = params[:start_at]
+    end
+    
+    if params[:end_at].nil? || params[:end_at].empty?
+      end_at = Time.now
+    else
+      end_at = params[:end_at]
+    end 
+    @temperatures = Temperature.where("sensor_id = ? AND checked_at BETWEEN ? AND ?", sensor_id, start_at, end_at)
     @temperatures_to_chart = @temperatures.collect { |t| t.value }
 
     @temperatures_to_chart = [0] unless @temperatures_to_chart
